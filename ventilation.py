92094164e97f628bc5a5083aa1085d81b56b5199
Shaunipotte <128689352+Shaunipotte@users.noreply.github.com>
2025-01-14 09:15:10 +0100

    Update ventilation.py

--- ventilation.py
+++ ventilation.py
@@ -4,13 +4,13 @@ pd.DataFrame(air, index=['Air'])
 
 Va = 3*4*8                   # m³, volume d'une pièce
 #FACADE SUD
-ACH_S = 0.5                     # 1/h, air changes per hour
+ACH_S = 0.25                     # 1/h, air changes per hour
 Va_dotS = ACH_S / 3600 * Va    # m³/s, infiltration "instantanée"
 #ENTRE PIECES
 ACH_I = 0.25                    # 1/h, air changes per hour
 Va_dotI = ACH_I / 3600 * Va    # m³/s, infiltration "instantanée"
 #FACADE NORD
-ACH_N = 0.25                    # 1/h, air changes per hour
+ACH_N = 0.5                    # 1/h, air changes per hour
 Va_dotN = ACH_N / 3600 * Va    # m³/s, infiltration "instantanée"
 
 # ventilation & infiltration
