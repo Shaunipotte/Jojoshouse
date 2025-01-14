639653d7579213f738239b1b003699a69fa55ed2
Shaunipotte <128689352+Shaunipotte@users.noreply.github.com>
2025-01-14 10:04:45 +0100

    changement ACH

--- ventilation.py
+++ ventilation.py
@@ -5,13 +5,13 @@ air['Capacity'] = air['Density'] * air['Specific heat'] * air['Volume']
 pd.DataFrame(air, index=['Air'])
 
 #FACADE SUD
-ACH_S = 0.25                     # 1/h, air changes per hour
+ACH_S = 2                     # 1/h, air changes per hour
 Va_dotS = ACH_S / 3600 * air['Volume']    # m³/s, infiltration "instantanée"
 #ENTRE PIECES
-ACH_I = 0.25                    # 1/h, air changes per hour
+ACH_I = 2                    # 1/h, air changes per hour
 Va_dotI = ACH_I / 3600 * air['Volume']    # m³/s, infiltration "instantanée"
 #FACADE NORD
-ACH_N = 0.5                    # 1/h, air changes per hour
+ACH_N = 4                    # 1/h, air changes per hour
 Va_dotN = ACH_N / 3600 * air['Volume']    # m³/s, infiltration "instantanée"
 
 # ventilation & infiltration
