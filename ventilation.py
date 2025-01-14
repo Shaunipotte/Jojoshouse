5d74f4fd96a7c3c7c95c0ee4a10c53cd9c30b3fd
Shaunipotte <128689352+Shaunipotte@users.noreply.github.com>
2025-01-14 09:19:09 +0100

    Update ventilation.py

--- ventilation.py
+++ ventilation.py
@@ -1,17 +1,18 @@
 air = {'Density': 1.2,                      # kg/m³
        'Specific heat': 1000}               # J/(kg·K)
+air['Volume'] = 3*4*8
+air['Capacity'] = air['Density'] * air['Specific heat'] * air['Volume']
 pd.DataFrame(air, index=['Air'])
 
-Va = 3*4*8                   # m³, volume d'une pièce
 #FACADE SUD
 ACH_S = 0.25                     # 1/h, air changes per hour
-Va_dotS = ACH_S / 3600 * Va    # m³/s, infiltration "instantanée"
+Va_dotS = ACH_S / 3600 * air['Volume']    # m³/s, infiltration "instantanée"
 #ENTRE PIECES
 ACH_I = 0.25                    # 1/h, air changes per hour
-Va_dotI = ACH_I / 3600 * Va    # m³/s, infiltration "instantanée"
+Va_dotI = ACH_I / 3600 * air['Volume']    # m³/s, infiltration "instantanée"
 #FACADE NORD
 ACH_N = 0.5                    # 1/h, air changes per hour
-Va_dotN = ACH_N / 3600 * Va    # m³/s, infiltration "instantanée"
+Va_dotN = ACH_N / 3600 * air['Volume']    # m³/s, infiltration "instantanée"
 
 # ventilation & infiltration
 Gvent_S = air['Density'] * air['Specific heat'] * Va_dotS
